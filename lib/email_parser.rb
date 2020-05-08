5938da2ca870fb03137d5a6978a131cffc963869
Thomas John Helin <thomashelin@yahoo.com>
2020-05-08 16:30:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
 
    def parse
      email_array = @email.split(/[, ]/).uniq #string objects in ruby have a method called split, it can cut up a string into pieces along a pre-defined string or regex, returning an array of smaller strings.
-     email_array.reject! {|element| element.empty?} #reject returns a new array containing the items in self for which the given block is not true. #
+     email_array.reject! {|element| element.empty?} #reject returns a new array containing the items in self for which the given block is not true. #empty? is a standard Ruby method on some objects like Arrays, Hashes and Strings. Its exact behaviour will depend on the specific object, but typically it returns true if the object contains no elements.
      email_array
      #binding.pry
    end
