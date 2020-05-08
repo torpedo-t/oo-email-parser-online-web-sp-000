6fbe7e40532733b390caec6ac548b4e3bbac8adc
Thomas John Helin <thomashelin@yahoo.com>
2020-05-08 16:27:15 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,8 +10,8 @@ class EmailAddressParser
    end
 
    def parse
-     email_array = @email.split(/[, ]/).uniq
-     email_array.reject! {|element| element.empty?}
+     email_array = @email.split(/[, ]/).uniq #string objects in ruby have a method called split, it can cut up a string into pieces along a pre-defined string or regex, returning an array of smaller strings.
+     email_array.reject! {|element| element.empty?} #reject returns a new array containing the items in self for which the given block is not true. 
      email_array
    end
 end
