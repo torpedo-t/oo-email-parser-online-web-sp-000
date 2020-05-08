eba57df3c7d0e3322b44cb7e50a74cd0b780f4dc
Thomas John Helin <thomashelin@yahoo.com>
2020-05-08 16:27:52 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,6 +2,7 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+require 'pry'
 class EmailAddressParser
    attr_accessor :email
 
@@ -11,7 +12,8 @@ class EmailAddressParser
 
    def parse
      email_array = @email.split(/[, ]/).uniq #string objects in ruby have a method called split, it can cut up a string into pieces along a pre-defined string or regex, returning an array of smaller strings.
-     #email_array.reject! {|element| element.empty?} #reject returns a new array containing the items in self for which the given block is not true. 
+     #email_array.reject! {|element| element.empty?} #reject returns a new array containing the items in self for which the given block is not true.
      email_array
+     binding.pry
    end
 end
